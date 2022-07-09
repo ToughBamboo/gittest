5c776003c754ce06e59c282d217c84db62c469f9
ToughBamboo <1187571163@qq.com>
2022-07-09 18:45:48 +0800

    first commit

---
+++ src/main/java/com/xhx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xhx.git;
+
+/**
+ * @Author: 韧竹
+ * @Date: 2022/07/09/18:35
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
