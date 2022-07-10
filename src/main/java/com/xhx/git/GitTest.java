e6321720512e884f83b5053c847ae8010bc737b5
ToughBamboo <1187571163@qq.com>
2022-07-10 14:46:45 +0800

    second commit

--- src/main/java/com/xhx/git/GitTest.java
+++ src/main/java/com/xhx/git/GitTest.java
@@ -8,5 +8,8 @@ package com.xhx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git2");
+
+
     }
 }
